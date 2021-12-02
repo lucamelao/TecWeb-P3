d2d139af5662603090faa0e1bec539bb498a06fb
pauloschade <paulosc4@al.insper.edu.br>
2021-12-02 16:44:14 -0300

    deployed to rinkeby and mumbai

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,28 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    mumbai: {
+      provider: () => new HDWalletProvider("55e9955dab796892a43a706b6d3d4bd199b1778e2619755fcd4ea844c05f0c7b", `https://matic-mumbai.chainstacklabs.com/`),
+      network_id: 80001,
+      gas: 6000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gasPrice: 10000000000,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider("55e9955dab796892a43a706b6d3d4bd199b1778e2619755fcd4ea844c05f0c7b", `https://rinkeby.infura.io/v3/e72e52471a414e1cbaaeaa3c60763693`),
+      network_id: 4,
+      gas: 6000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gasPrice: 10000000000,
     },
   },
   contracts_directory: './src/contracts/',
